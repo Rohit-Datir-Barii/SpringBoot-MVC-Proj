1c2d65fe31172d0f7230ca7ffdc84cc4a7638989
Rohit-Datir-Barii <rohitdatir143@gmail.com>
2023-04-16 13:38:08 +0530

    Adding setter and getter

--- src/main/java/com/rd/entity/Employee.java
+++ src/main/java/com/rd/entity/Employee.java
@@ -7,11 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Data;
-
 @Entity
 @Table(name="emp")
-@Data
 public class Employee {
 	
 	@Id
@@ -22,4 +19,40 @@ public class Employee {
 	@Column(length = 20)
 	private  String  job;
 	private  Float sal;
+	public Integer getEmpno() {
+		return empno;
+	}
+	public void setEmpno(Integer empno) {
+		this.empno = empno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public Float getSal() {
+		return sal;
+	}
+	public void setSal(Float sal) {
+		this.sal = sal;
+	}
+	public Employee(Integer empno, String ename, String job, Float sal) {
+		super();
+		this.empno = empno;
+		this.ename = ename;
+		this.job = job;
+		this.sal = sal;
+	}
+	@Override
+	public String toString() {
+		return "Employee [empno=" + empno + ", ename=" + ename + ", job=" + job + ", sal=" + sal + "]";
+	}
+	
 }
